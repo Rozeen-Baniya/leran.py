e3ae5355e0eef14d91e2a8fed3bdccf661de0072
rojinbaniya <111887404+Rozeen-Baniya@users.noreply.github.com>
2025-04-22 17:48:42 +0545

    Add files via upload
    this is some of the programs of python.

---
+++ day7.py
@@ -0,0 +1,50 @@
+# if 3 > 3:
+#     print ("this is world")
+# if 4 > 5: 
+#     print("this is 1st elif")  #this is an intendation use tab to give an indentaion
+# else:
+#     print("this is not true")
+# print("You are not good")
+
+
+# fn = 'ram'
+# ln = 'Hari'
+# age = 29
+
+# if fn == 'umesh':
+#     if age > 30:
+#         print("You are old")
+#     elif age > 20 and age < 30:
+#         print('Young')
+#     else:
+#         print('teen')
+# elif ln == 'regmi':
+#     if fn == 'umesh':
+#         print('Hello umesh regmi')
+#     else: 
+#         print('I do not know your first name')
+# else:
+#     print('I do not know your name at all')
+
+
+# print('rojin') if 3>2 else print('baniya')
+
+# a = int(input('Enter a number '))
+# print(a)
+# print(type(a))
+
+
+# using fstring
+# a = int(input('Enter a number: '))
+# if a % 2 == 0:
+#     print(f'The number {a} is even')
+# else:
+#     print(f'The number {a} is odd')
+
+
+# using str 
+# a = int(input('Enter a number: '))     
+# if a % 2 == 0:
+#     print(str(a), 'is even')
+# else:
+#     print(str(a), 'is odd')
---
+++ day8.py
@@ -0,0 +1,101 @@
+#OOp
+
+# dry principle--- do not repeat yourself
+
+# i = 0  
+# while i < 5: # this is while loop 
+#     print('hello world')
+#     i = i + 1
+# print('this is the end')
+
+# i = 0
+# while i < 5:
+#     print('Hello world')
+#     i = i + 1 
+#     break
+#     print('this is afte break')
+# print('I am end')
+
+# i = 0
+# while i < 5:
+#     print('Hello world')
+#     i = i + 1 
+#     continue # mathi ko matra continue garney
+#     print('this is afte break')
+# print('I am end')
+
+
+# names = ['ram' , 'sita', 'hari', 'shyam']
+
+# i = 0
+# while i < len(names):
+#     print(f'hi {names[i].capitalize()}')
+#     i = i + 1
+
+# num = input("enter a number ")
+# i = 2
+
+# while i <= num:
+#     print(i)
+#     i = i + 2
+
+# for variable_name in iterables:
+# statements
+
+
+# string list set tuple dictionary range
+
+# print( list (range(10)))
+# print( list (range(2:10)))
+# print( list (range(2:10:2)))
+
+# ffor i in 'umesh';
+# print(i)
+
+# for item in ['ram', 'hari', 'shyam', 'sita']:
+#     print(item)
+
+# string list set tuple dictionary range
+
+# grades = {'ram': 20, 'hari':40, 'sita':25}
+
+# for item in grades:
+#     print(item)
+
+# for item in grades.keys():
+#     print(item)
+
+# for item in grades.values():
+#     print(item)
+
+#string list set tuple dictionary range
+
+# grades = { 'ram':20 , 'shyam':30 , 'hari': 80}
+
+# print(grades.items())
+# for k, v in grades.items():
+#     print(k, v)
+
+# for num in range(10):
+#     print(num)
+
+# k, v = ('ram', 20)
+
+# for num in range(10):
+#     if num % 2 == 0:
+#         print(num)
+#     else:
+#         continue
+#         print("hi")
+
+
+# ram 85
+# shyam 62
+# hari 55
+
+# neter your name ram Ram rAM Ram ram 
+
+# >= 80 Hi ram your grade is .... you passed with distinction
+# >= 60 and < 80 Hi your grade is ... you passed with first division 
+# < 50 mom's flying chapppal receive dat 15km/hr
+
