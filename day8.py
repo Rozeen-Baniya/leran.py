05c82295c8f7e0098b06de4f3f6834462d6ebe74
rojinbaniya <111887404+Rozeen-Baniya@users.noreply.github.com>
2025-04-24 05:50:17 +0545

    Update day8.py

--- day8.py
+++ day8.py
@@ -99,3 +99,12 @@
 # >= 60 and < 80 Hi your grade is ... you passed with first division 
 # < 50 mom's flying chapppal receive dat 15km/hr
 
+name = input('Enter your name: ')
+grade = int(input('Enter your grade: '))
+
+if grade >= 80:
+    print(f'Hi {name}, your grade is {grade}. You passed with distinction.')
+elif grade >= 50 and grade < 80:
+    print(f'Hi {name}, your grade is {grade}. You passed with first division.')
+else: 
+    print(f'Mom’s flying chappal received at {grade} km/hr!')
