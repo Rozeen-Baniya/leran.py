ad1f54de6f5eaa1ef0d2af22a64537e9f559b7ef
rojinbaniya <111887404+Rozeen-Baniya@users.noreply.github.com>
2025-05-22 18:31:06 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# import constants 
+# print(constants.PI)
+# print(constants.GRAVITY)
+
+# form import constants import PI,  Gravity
+# print(PI)
+
+# import arithmetic
+
+# arithmetic.add()
+
+# from arithmetic import add
+
+# add()
