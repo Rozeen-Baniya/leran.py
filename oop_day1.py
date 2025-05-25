7c011b49e9252c8ebee32546bca7a0592884df8e
rojinbaniya <111887404+Rozeen-Baniya@users.noreply.github.com>
2025-05-25 10:19:05 +0545

    Add files via upload

---
+++ oop_day1.py
@@ -0,0 +1,33 @@
+# class is a blueprint of an object
+# oject is the instantion of an Class
+# Class is a model/ template of an object
+# Object is a real world example of a class
+# Class defines how an object looks like
+# oop to use real life example 
+# clsss is an user defined data type
+
+
+# the use of init is that it creates the object in the memory and initializes the property of the object
+# init method defines the size of an object
+
+
+# class student:
+#     def __init__(self):
+#         self.first_name = 'rojin'
+#         self.last_name = 'nalaju'
+#         self.address = 'balaju'
+
+# s = student()
+# a = student()
+
+
+# class student:
+#     def __init__(self, fn, ln, add):
+#         self.first_name = fn
+#         self.last_name = ln
+#         self.address = add
+
+# s = student('ram', 'hari', 'ktm')
+# a = student('umesh', 'regmi', 'balaju')
+
+# print(type(a))
