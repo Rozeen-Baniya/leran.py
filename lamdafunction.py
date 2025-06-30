e771cbf5990d8d7dad74804116b075e6b7360023
rojinbaniya <111887404+Rozeen-Baniya@users.noreply.github.com>
2025-06-30 10:02:57 +0545

    Add files via upload

---
+++ lamdafunction.py
@@ -0,0 +1,8 @@
+
+# lambda n: True if n % 2 == 0 else False # This is anonomyous lamda function
+
+
+# s = lambda n: True if n % 2 == 0 else False # This is not anonomyous lamda function
+
+s = lambda a, b: a+b
+print(s(3,2))
